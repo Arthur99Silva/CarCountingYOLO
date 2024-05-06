1ec9caeba13d4f50bf48ff36825825ae8cbb1d9f
Arthur99Silva <arthurassilva99@gmail.com>
2024-05-06 12:47:46 -0300

    Cars

--- Car-Counter.py
+++ Car-Counter.py
@@ -5,10 +5,14 @@ import math
 import time
 from sort import*
 
-cap = cv2.VideoCapture("C:/Users/Arthur/Documents/FaceDetection/FacialDetection/Videos/cars3.mp4")  # Diretorio p/ video
+cap = cv2.VideoCapture("../CarCountingYOLO/CarCountingYOLO/Cars/cars.mp4")  # Diretorio p/ video
 
 # Tracking
 tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+
+limits = [225, 400, 1020, 400]
+
+totalCount = 0
  
 model = YOLO("../Yolo-Weights/yolov8n.pt")
  
@@ -42,20 +46,29 @@ while True:
                 currrenClass = classNames[cls]
                 if currrenClass == "car" or currrenClass == "truck" or currrenClass == "bus" or currrenClass == "motorbike" and conf > 0.3:
                     # cvzone.putTextRect(img, f'{currrenClass} {conf}', (max(0, x1), max(35, y1)), scale=0.6, thickness=1, offset=3)
-                    cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt= 5)
+                    # cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt= 5)
                     currentArray = np.array([x1, y1, x2, y2, conf])
                     detections = np.vstack((detections, currentArray))
 
     resultsTracker = tracker.update(detections)
+
+    cv2.line(img,(limits[0], limits[1]), (limits[2], limits[3]), (0, 0, 255), 5)
 
     for result in resultsTracker:
         x1, y1, x2, y2, id = result
         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
         print(result)
         w, h = x2 - x1, y2 - y1
-        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255, 0, 0))
+        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255, 0, 255))
         cvzone.putTextRect(img, f'{id}', (max(0, x1), max(35, y1)), scale=2, thickness=3, offset=10)
 
+        cx, cy = x1+w//2, y1+h//2
+        cv2.circle(img,(cx, cy), 5, (255, 0, 255), cv2.FILLED)
+
+        if limits[0] < cx < limits[2] and limits[1] - 20 < cy < limits[1] + 20:
+            totalCount += 1
+
+    cvzone.putTextRect(img, f'Count: {totalCount}', (50, 50))
 
     cv2.imshow("Image", img)
     #cv2.imshow("ImageRegion", imgRegion)
